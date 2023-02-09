f341e48c8985b425d43cff888bd911d4d3de3ef7
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2023-02-09 07:35:40 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -134,7 +134,7 @@ html_theme_options = {
     ##
     "collapse_navigation": True,
     "sticky_navigation": True,
-    "navigation_depth": 2,
+    "navigation_depth": 3,
     "includehidden": True,
     "titles_only": False,
 }
